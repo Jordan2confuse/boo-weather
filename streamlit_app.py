e0900dd920d9744e01a09468cfc162e3d45c3038
Jordan2confuse <jordanboo486@gmail.com>
2025-01-04 01:34:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as st
 
 st.title(' 🌀🌡️☁️ Weather Alert System')
 
 st.info('Get your latest weather updates!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Jordan2confuse/boo-weather/refs/heads/master/weather_prediction_dataset.csv')
+df
